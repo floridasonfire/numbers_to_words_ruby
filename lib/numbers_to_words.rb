e390c7972d81eb734985a038e0009c188f315607
Brent Bailey & Jessica Engel <student@epicodus.com>
2015-04-21 16:48:21 -0700

    Meet three of four specs, work on transitioning to variables

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -2,52 +2,91 @@ class Fixnum
 
 define_method (:numbers_to_words) do
 
-number_hash_1={1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine", 10=>"ten", 11=>"eleven", 12=>"twelve", 13=>"thirteen", 14=>"fourteen", 15=>"fifteen", 16=>"sixteen", 17=>"seventeen", 18=>"eighteen", 19=>"nineteen", 20=>"twenty", 30=> "thirty", 40=> "forty", 50=>"fifty", 60=>"sixty", 70=>"seventy", 80=>"eighty", 90=>"ninety", 100=>"one hundred ", 200=>"two hundred ", 300=>"three hundred ", 400=>"four hundred ", 500=>"five hundred ", 600=>"six hundred ", 700=>"seven hundred ", 800=>"eight hundred", 900=>"nine hundred"}
+hash_1={0 => "", 1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine", 10=>"ten", 11=>"eleven", 12=>"twelve", 13=>"thirteen", 14=>"fourteen", 15=>"fifteen", 16=>"sixteen", 17=>"seventeen", 18=>"eighteen", 19=>"nineteen" ,20=>"twenty"}
 
 
-number_hash_3=
+hash_2=
 {2=>"twenty-", 3=>"thirty-", 4=>"forty-", 5=>"fifty-", 6=>"sixty-", 7=>"seventy-", 8=>"eighty-", 9=>"ninety-"}
 
-
-number_hash_5= {1=>"one hundred ", 2=>"two hundred ", 3=>"three hundred ", 4=>"four hundred ", 5=>"five hundred ", 6=>"six hundred ", 7=>"seven hundred ", 8=>"eight hundred", 9=>"nine hundred" }
+hash_3 =
+{1 =>"ten", 2=>"twenty", 3=>"thirty", 4=>"forty", 5=>"fifty", 6=>"sixty", 7=>"seventy", 8=>"eighty", 9=>"ninety"}
 
 result=" "
 input_array = []
 output_array = []
 input_array = self.to_s.split('')
+
 input_array.each() do |string|
   output_array.push(string.to_i())
 end
-  if number_hash_1.include?(self)
-    result=number_hash_1.fetch(self)
 
+  if hash_1.include?(self)
+    result=hash_1.fetch(self)
 
+##Two digit numbers
   elsif (self.to_s()).length()==2
+    first_digit2= hash_2.fetch(output_array.at(0))
+    second_digit2= hash_1.fetch(output_array.at(1))
 
-    result = number_hash_3.fetch(output_array.at(0)) +    number_hash_1.fetch(output_array.at(1))
+    last_two_digits_unique2= hash_3.fetch(output_array.at(0))
+    if output_array.at(1)!=0
+    result = first_digit2 + second_digit2
+    else
+      result=last_two_digits_unique2
+    end
 
+##Three digit numbers
   elsif (self.to_s).length()==3
+    first_digit3= hash_1.fetch(output_array.at(0))
+    second_digit3= hash_2.fetch(output_array.at(1))
+    third_digit3= hash_1.fetch(output_array.at(0))
 
-      if output_array.at(1) ==1
-        result = number_hash_5.fetch(output_array.at(0)) + number_hash_1.fetch(((output_array.at(1).to_s).concat((output_array.at(2).to_s))).to_i)
+    last_two_digits_unique3= (hash_1.fetch(output_array.at(0)) + "hundred" + hash_1.fetch((input_array.at(1) +
+    input_array.at(2)).to_i))
+
+      if output_array.at(1) !=0 && output_array.at(2) == 0
+        result = hash_1.fetch(output_array.at(0)) + " hundred " + hash_3.fetch(output_array.at(1))
       elsif  output_array.at(2)!=0 && output_array.at(1)!=0
-        result = number_hash_5.fetch(output_array.at(0)) + number_hash_3.fetch(output_array.at(1)) + number_hash_1.fetch(output_array.at(2))
+        result = hash_1.fetch(output_array.at(0)) + " hundred " + hash_2.fetch(output_array.at(1)) + hash_1.fetch(output_array.at(2))
       else
-        result = number_hash_1.fetch(output_array.at(0))+number_hash_1.fetch(output_array(1))
+        result = hash_1.fetch(output_array.at(0)) + " hundred"
       end
 
+##Four digit numbers
   elsif (self.to_s).length()==4
 
-      if output_array.at(1)+output_array.at(2)+output_array.at(3)==0
-        result = number_hash_1.fetch(output_array.at(0))+" thousand"
-      elsif output_array.at(2)+output_array.at(3)==0
-        result=number_hash_1.fetch(output_array.at(0))+ " thousand " +number_hash_5.fetch(output_array.at(1))
+    third_digit4= hash_2.fetch(output_array.at(2))
+    fourth_digit4= hash_1.fetch(output_array.at(3))
+
+    last_two_digits_unique4= (hash_1.fetch(output_array.at(0))+ " thousand " + hash_1.fetch(output_array.at(1)) + "hundred" + hash_1.fetch((input_array.at(2) + input_array.at(3)).to_i))
+
+    last_two_digits_unique_4_dashes= (hash_1.fetch(output_array.at(0))+ " thousand " + hash_1.fetch(output_array.at(1)) + "hundred" + hash_2.fetch(output_array.at(2))+hash_1.fetch(output_array.at(3)))
+
+## if suffix is -000
+  if output_array.at(1)+output_array.at(2)+output_array.at(3)==0
+        result = hash_1.fetch(output_array.at(0))+" thousand"
+## if suffix is -00
+    elsif output_array.at(2)+output_array.at(3)==0
+        result=hash_1.fetch(output_array.at(0))+ " thousand " +hash_1.fetch(output_array.at(1)) + " hundred"
+##if number is -00-
+    elsif output_array.at(1)+output_array.at(2)
+## if last 3 are greater than 100
+      elsif output_array.at(1)!=0
+          result=hash_1.fetch(output_array.at(0))+ " thousand " + hash_2.fetch(output_array.at(2)) + hash_1.fetch(output_array.at(3))
+##if last is greater than 0
+        elsif output_array.at(3)!=0
+          result=hash_1.fetch(output_array.at(0))+ " thousand "+ hash_1.fetch(output_array.at(1)) + " hundred " + hash_3.fetch(output_array.at(2))
+        elsif output_array.at(2)==1
+          result=hash_1.fetch(output_array.at(0))+ " thousand "+ hash_1.fetch(output_array.at(1)) + " hundred " +hash_1.fetch(input_array.at(2).concat(input_array.at(3)).to_i)
+        else
+          result=hash_1.fetch(output_array.at(0))+ " thousand "+ hash_1.fetch(output_array.at(1)) + " hundred " +hash_3.fetch(output_array.at(2)) + hash_1.fetch(output_array.at(3))
+        end
+
+      else
 
       end
 
-  else
 
-    end
   result
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,5 +1,6 @@
 require ('rspec')
 require ('numbers_to_words.rb')
+require('pry')
 
 describe('Fixnum#numbers_to_words') do
   it("takes the numbers 0-19 and returns their written form") do
@@ -7,14 +8,14 @@ describe('Fixnum#numbers_to_words') do
   end
 
   it("takes the numbers 20-99 and returns their written form") do
-    expect(25.numbers_to_words()).to(eq("twenty-five"))
+    expect(20.numbers_to_words()).to(eq("twenty"))
   end
 
   it("takes the numbers 100-999 and returns their written form") do
-    expect(333.numbers_to_words()).to(eq("three hundred thirty-three"))
+    expect(120.numbers_to_words()).to(eq("one hundred twenty"))
   end
 
   it("takes the numbers 1000-9999 and returns their written form") do
-    expect(1100.numbers_to_words()).to(eq("one thousand one hundred "))
+    expect(1123.numbers_to_words()).to(eq("one thousand one hundred twenty-three"))
   end
 end
